30f5fb4044219cd851b4a070157b65bfe605f3bc
alexgoni <goni000211@gmail.com>
2024-03-03 17:40:50 +0900

    feat: shared component 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 import classNames from 'classnames/bind'
+import { useEffect, useState } from 'react'
 import styles from './App.module.scss'
+import FullScreenMessage from './components/shared/FullScreenMessage'
 
 const cx = classNames.bind(styles)
 
 function App() {
+  const [wedding, setWedding] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+
+    fetch('http://192.168.0.24:8888/wedding')
+      .then((response) => {
+        if (!response.ok) throw new Error('청첩장 정보를 불러오지 못했습니다.')
+        return response.json()
+      })
+      .then((data) => {
+        setWedding(data)
+      })
+      .catch((e) => {
+        console.log(e)
+        setError(true)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
+  if (loading) return <FullScreenMessage type="loading" />
+  if (error) return <FullScreenMessage type="error" />
+
   return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className={cx('container')}>{JSON.stringify(wedding)}</div>
+    </>
   )
 }
 
