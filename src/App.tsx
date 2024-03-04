857882528f3106e43f81fa70efe18a89451cc6a3
alexgoni <goni000211@gmail.com>
2024-03-04 21:42:29 +0900

    feat: 지도 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import ImageGallery from './components/sections/ImageGallery'
 import Intro from './components/sections/Intro'
 import Invitation from './components/shared/Invitation'
 import Calendar from './components/sections/Calendar'
+import Map from './components/sections/Map'
 
 const cx = classNames.bind(styles)
 
@@ -66,6 +67,7 @@ function App() {
         <Invitation message={invitation} />
         <ImageGallery images={galleryImages} />
         <Calendar date={date} />
+        <Map location={location} />
         {JSON.stringify(wedding)}
       </div>
     </>
